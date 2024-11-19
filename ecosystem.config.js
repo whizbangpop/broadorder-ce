0a864d1f2fd1f8142e660674b00f8ee479c40332
whizbangpop <101482788+whizbangpop@users.noreply.github.com>
2024-11-19 19:44:01 +0000

    pm2

---
+++ ecosystem.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+    apps : [{
+      name :"broadorder-ce",
+      script : "npm run start",
+      error_file : "./pm2-error.log",
+      out_file : "./pm2-out.log",
+    }]
+  }
